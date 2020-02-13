9e1f08cbd2be6304efe4109715bdf2549a0076e8
Christian Baehr <cbaehr@aiddata.wm.edu>
2020-02-13 14:45:36 -0500

    moved r script into new folder

--- R/operation.R
+++ R/operation.R
@@ -1,4 +1,11 @@
-
+#' Perform some operation
+#'
+#' Given two input values, perform some arithmetic operation on them.
+#'
+#' @param x The first input value
+#' @param y The second input value. Should have the same dimensionality as first.
+#' @param op The arithmetic operation to be run on x and y.
+#' @export
 operation <- function(x, y, op) {
   
   if(!op %in% c("add", "subtract", "multiply", "divide")) {
